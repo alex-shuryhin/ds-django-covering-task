d1beb5e7b6d5d295f91f760ce3c313627c7569c9
Alex <shurygin@ukr.net>
2016-04-25 20:04:10 +0300

    alex

--- ds_django_covering_task/urls.py
+++ ds_django_covering_task/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^secure-admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^secure-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^book_store/', include('book_store.urls', namespace="book_store")),
-
-
 )
