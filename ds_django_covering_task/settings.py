fd57dd0a621c823e26ec278883fbb4471754b1d2
Alex <shurygin@ukr.net>
2016-04-25 20:26:27 +0300

    20.26

--- ds_django_covering_task/settings.py
+++ ds_django_covering_task/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Environment variable defined in local.py / production.py"
+SECRET_KEY = "r&fn%$54(ib5y*-208a4pc6*tns#5g)c+c66$37^x5%d+zu8ed"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- ds_django_covering_task/settings/local.py
+++
@@ -1,4 +0,0 @@
-from .base import *
-
-#SECRET_KEY = get_env_variable("SECRET_KEY")
-SECRET_KEY = "r&fn%$54(ib5y*-208a4pc6*tns#5g)c+c66$37^x5%d+zu8ed"
