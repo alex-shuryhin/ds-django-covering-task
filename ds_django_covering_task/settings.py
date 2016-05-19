29006d0cc13e21394dfa7f11e5a0ed91d9824287
alex-shuryhin <shurygin@ukr.net>
2016-05-20 00:20:23 +0300

    Update settings.py

--- ds_django_covering_task/settings.py
+++ ds_django_covering_task/settings.py
@@ -28,7 +28,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
